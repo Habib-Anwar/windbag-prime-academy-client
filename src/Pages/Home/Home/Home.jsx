f923c83d51d01c2e49455f18a038d7b03946a5f0
Habib-Anwar <121849760+Habib-Anwar@users.noreply.github.com>
2023-06-07 20:05:02 +0600

    popular classes section

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,10 +1,16 @@
 import Banner from "../Banner/Banner";
+import PopularClasses from "../PopularClasses/PopularClasses";
+
+
+
+
 
 
 const Home = () => {
     return (
         <div>
          <Banner></Banner>
+        <PopularClasses></PopularClasses>
         </div>
     );
 };
--- src/Pages/Home/PopularClasses/PopularClasses.jsx
+++ src/Pages/Home/PopularClasses/PopularClasses.jsx
@@ -1,9 +1,25 @@
+import { useEffect, useState } from "react";
+import Courses from "../Courses/Courses";
 
 
 const PopularClasses = () => {
+    const [courses, setCourses] = useState([]);
+    useEffect(()=>{
+        fetch('classes.json')
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            const popularCourses = data.filter(course => course.category === 'popular');
+            setCourses(popularCourses)
+        })
+    }, [])
     return (
         <div>
-            
+            {
+                courses.map(course => <Courses
+                key={course.id}
+                course={course}></Courses>)
+            }
         </div>
     );
 };
