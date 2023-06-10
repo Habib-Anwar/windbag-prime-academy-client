ed342d51af59e9599a39319ae8e5a5878edc280e
Habib-Anwar <121849760+Habib-Anwar@users.noreply.github.com>
2023-06-11 02:56:25 +0600

    show table data

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -7,21 +7,20 @@ const Dashboard = () => {
         
 <>
 <div className="text-center">
-   <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800" type="button" data-drawer-target="drawer-navigation" data-drawer-show="drawer-navigation" aria-controls="drawer-navigation">
-   Show navigation
-   </button>
+  
 </div>
-<Outlet></Outlet>
+
 <div className="drawer">
   <input id="my-drawer" type="checkbox" className="drawer-toggle" />
-  <div className="drawer-content">
-    <label htmlFor="my-drawer" className="btn btn-primary drawer-button">Open drawer</label>
+  <div className="drawer-content flex flex-col items-center justify-center">
+    <label htmlFor="my-drawer" className="btn btn-primary drawer-button">Open Menu</label>
+    <Outlet></Outlet>
   </div> 
   <div className="drawer-side">
     <label htmlFor="my-drawer" className="drawer-overlay"></label>
     <ul className="menu p-4 w-80 h-full bg-base-200 text-base-content">
       <li><Link to='/'><FaHome></FaHome>Home</Link></li>
-      <li><Link><FaLandmark></FaLandmark>My Selected Classes</Link></li>
+      <li><Link to='/dashboard/studentdashboard'><FaLandmark></FaLandmark>My Selected Classes</Link></li>
       <li><Link><FaBook></FaBook> My Enrolled Classes</Link></li>
       <li><Link><FaWallet></FaWallet> Payment History</Link></li>
       
